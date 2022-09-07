cde76e4c3644ac0c5ba5b7eee8c47e3352db2cf5
fervillalbag <21fervillalbag@gmail.com>
2022-09-07 00:11:03 -0400

    feat: create getAll and getUser from api

--- apps/dafne/src/app/app.module.ts
+++ apps/dafne/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { MessagesModule } from '../messages/messages.module'
 import { PrismaService } from '../../prisma/prisma.service'
+import { UsersService } from '../users/users.service'
+import { UsersController } from '../users/users.controller'
 
 @Module({
   imports: [MessagesModule],
-  controllers: [AppController],
-  providers: [AppService, PrismaService],
+  controllers: [AppController, UsersController],
+  providers: [AppService, PrismaService, UsersService],
 })
 export class AppModule {}
