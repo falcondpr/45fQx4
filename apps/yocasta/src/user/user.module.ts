61129782b1d947d5fa22f4bd0f38443e65289193
Fernando Villalba <21fervillalbag@gmail.com>
2022-10-08 15:45:09 -0300

    feat: login, register and update user api

--- apps/yocasta/src/user/user.module.ts
+++ apps/yocasta/src/user/user.module.ts
@@ -3,12 +3,13 @@ import { UserService } from './user.service'
 import { UserController } from './user.controller'
 import { MongooseModule } from '@nestjs/mongoose'
 import { User, UserSchema } from './schema/user.schema'
+import { JwtService } from '@nestjs/jwt'
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
-  providers: [UserService],
+  providers: [UserService, JwtService],
   controllers: [UserController],
 })
 export class UserModule {}
--- apps/yocasta/src/user/user.service.ts
+++ apps/yocasta/src/user/user.service.ts
@@ -1,23 +1,74 @@
+import * as argon from 'argon2'
 import { Model } from 'mongoose'
 import { InjectModel } from '@nestjs/mongoose'
-import { Injectable } from '@nestjs/common'
+import {
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
 
-import { CreateUserDTO } from './dto/create-user.dto'
+import { CreateUserDTO, LoginUserDTO } from './dto/create-user.dto'
 import { UpdateUserDTO } from './dto/update-user.dto'
 import { User, UserDocument } from './schema/user.schema'
+import { SECRET_PASS } from '../utils/constans'
 
 @Injectable()
 export class UserService {
-  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+  constructor(
+    @InjectModel(User.name) private userModel: Model<UserDocument>,
+    private jwtService: JwtService,
+  ) {}
 
-  async create(dto: CreateUserDTO): Promise<User> {
-    const user = new this.userModel(dto)
-    return await user.save()
+  async signInToken(userId: string, email: string, name: string) {
+    const payload = { id: userId, email, name }
+
+    const token = await this.jwtService.signAsync(payload, {
+      expiresIn: '72h',
+      secret: SECRET_PASS,
+    })
+    return token
+  }
+
+  async create(dto: CreateUserDTO): Promise<string> {
+    const hash = await argon.hash(dto.password)
+
+    const foundUser = await this.userModel.findOne({ email: dto.email })
+
+    if (foundUser) throw new ForbiddenException('El email ya existe')
+
+    const user = new this.userModel({
+      name: dto.name,
+      email: dto.email,
+      username: dto.username,
+      password: hash,
+    })
+    await user.save()
+
+    return await this.signInToken(user._id, user.email, user.name)
+  }
+
+  async login(dto: LoginUserDTO): Promise<string> {
+    const user = await this.userModel.findOne({ email: dto.email })
+    if (!user) throw new NotFoundException('El usuario no existe')
+
+    const pwMatches = await argon.verify(user.password, dto.password)
+    if (!pwMatches)
+      throw new ForbiddenException('Las credenciales no coinciden')
+
+    return await this.signInToken(user._id, user.email, user.name)
   }
 
   async update(id: string, dto: UpdateUserDTO): Promise<User> {
-    const user = await this.userModel.findByIdAndUpdate(id, dto, { new: true })
-    return user
+    const currentUser = dto
+    const findUser = await this.userModel.findById(id)
+    if (!findUser) throw new NotFoundException('Usuario no encontrado')
+
+    if (dto.password) {
+      currentUser.password = await argon.hash(currentUser.password)
+    }
+
+    return await this.userModel.findByIdAndUpdate(id, dto, { new: true })
   }
 
   async delete(id: string): Promise<User> {
