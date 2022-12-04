b8f2228a14b28e6ec63b6bc5fffb17c2f3cd28c0
Fernando Villalba <21fervillalbag@gmail.com>
2022-12-04 02:14:18 -0300

    feat: login and register screen ui

--- apps/kousa/src/router/navigation.tsx
+++ apps/kousa/src/router/navigation.tsx
@@ -4,6 +4,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 // screens
 import Home from '../screens/Home'
 import Auth from '../screens/Auth'
+import Register from '../screens/Register'
+import Login from '../screens/Login'
 
 const Navigation: React.FC = () => {
   return (
@@ -11,6 +13,8 @@ const Navigation: React.FC = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/auth" element={<Auth />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   )
--- apps/kousa/src/theme.ts
+++ apps/kousa/src/theme.ts
@@ -1,7 +1,11 @@
-import { extendTheme } from '@chakra-ui/react'
+import { extendTheme, color } from '@chakra-ui/react'
 
 // 2. Extend the theme to include custom colors, fonts, etc
 const colors = {
+  ...color,
+  primary: '#333333',
+  'light-gray': '#999999',
+  'border-color': '#bebebe',
   brand: {
     900: '#1a365d',
     800: '#153e75',
