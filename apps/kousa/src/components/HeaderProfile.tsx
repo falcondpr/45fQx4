1610aa58baeba77ce2b536b012e81c7c6f810b23
Fernando Villalba <21fervillalbag@gmail.com>
2022-12-04 15:10:52 -0300

    feat: header profile component

---
+++ apps/kousa/src/components/HeaderProfile.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Box, Flex, Image } from '@chakra-ui/react'
+import { Link, NavigateFunction } from 'react-router-dom'
+
+import TextUI from '../ui/Text'
+
+const HeaderProfile: React.FC<{
+  navigate: NavigateFunction
+  userAvatar: string
+}> = ({ navigate, userAvatar }) => {
+  return (
+    <Flex
+      alignItems={{ base: 'center' }}
+      justifyContent={{ base: 'space-between' }}
+      p="1.25rem"
+    >
+      <Box cursor="pointer" onClick={() => navigate('/profile')}>
+        <Image
+          w="2.5rem"
+          h="2.5rem"
+          objectFit="cover"
+          rounded="2.5rem"
+          src={userAvatar}
+          alt=""
+        />
+      </Box>
+      <Box>
+        <Link to="/tickets">
+          <TextUI p="5px" textTransform="uppercase" color="primary">
+            Tickets
+          </TextUI>
+        </Link>
+      </Box>
+    </Flex>
+  )
+}
+
+export default HeaderProfile
--- apps/kousa/src/screens/Home.tsx
+++ apps/kousa/src/screens/Home.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
 import { Box } from '@chakra-ui/react'
+import { useNavigate } from 'react-router-dom'
+
+import HeaderProfile from '../components/HeaderProfile'
 
 const Home: React.FC = () => {
-  return <Box>Home</Box>
+  const navigate = useNavigate()
+
+  return (
+    <Box>
+      <HeaderProfile
+        navigate={navigate}
+        userAvatar={'https://bit.ly/3FpDJGG'}
+      />
+    </Box>
+  )
 }
 
 export default Home
