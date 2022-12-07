f54c980e387abf90606cf2f0accfc58efe6335dd
Fernando Villalba <21fervillalbag@gmail.com>
2022-12-07 02:04:31 -0300

    feat: link active when current page is equal

--- apps/kousa/src/components/Navbar.tsx
+++ apps/kousa/src/components/Navbar.tsx
@@ -4,9 +4,12 @@ import { Box, Text } from '@chakra-ui/react'
 import { MdLocalGroceryStore } from 'react-icons/md'
 import { HiSearch, HiOutlineUserCircle } from 'react-icons/hi'
 import { TbMessageDots } from 'react-icons/tb'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 const Navbar: React.FC = () => {
+  const { pathname } = useLocation()
+  console.log(pathname)
+
   return (
     <Box
       borderTop="1px solid"
@@ -23,31 +26,41 @@ const Navbar: React.FC = () => {
       alignItems="center"
     >
       <Link to="/">
-        <Text fontSize={{ base: '2rem' }} color="primary">
+        <Text
+          fontSize={{ base: '2rem' }}
+          color={pathname === '/' ? 'primary' : 'border-color'}
+        >
           <MdLocalGroceryStore />
         </Text>
       </Link>
 
       <Link to="/search">
-        <Text fontSize={{ base: '2rem' }} color="border-color">
+        <Text
+          fontSize={{ base: '2rem' }}
+          color={pathname === '/search' ? 'primary' : 'border-color'}
+        >
           <HiSearch />
         </Text>
       </Link>
 
       <Link to="/">
-        <Text fontSize={{ base: '2rem' }} color="border-color">
-          {/* <TbMessageDots /> */}
-        </Text>
+        <Text fontSize={{ base: '2rem' }}>{/* <TbMessageDots /> */}</Text>
       </Link>
 
       <Link to="/">
-        <Text fontSize={{ base: '2rem' }} color="border-color">
+        <Text
+          fontSize={{ base: '2rem' }}
+          color={pathname === '/messages' ? 'primary' : 'border-color'}
+        >
           <TbMessageDots />
         </Text>
       </Link>
 
       <Link to="/">
-        <Text fontSize={{ base: '2rem' }} color="border-color">
+        <Text
+          fontSize={{ base: '2rem' }}
+          color={pathname === '/profile' ? 'primary' : 'border-color'}
+        >
           <HiOutlineUserCircle />
         </Text>
       </Link>
