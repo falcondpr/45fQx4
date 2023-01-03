0d0fa843e4c094a1ad18fd282a52df846e3ca185
Fernando Villalba <21fervillalbag@gmail.com>
2023-01-03 01:58:20 -0300

    feat: data structure backend

---
+++ structure/product.ts
@@ -0,0 +1,40 @@
+export type Product = {
+  id: string
+  name: string
+  description: string
+  price: number
+  owner_id: string
+  imagenes: Image[]
+  product_status: STATUS_PRODUCT
+  status: {
+    value: SALE_STATUS
+    updated_at: string
+  }
+  location: {
+    city: string
+  }
+  tags: {
+    text: string
+    slug: string
+  }
+  created_at: string
+  updated_at: string
+}
+
+type Image = {
+  id: string
+  url_image: string
+  created_at: string
+}
+
+enum SALE_STATUS {
+  SOLD_OUT,
+  AVAILABLE,
+  INVISIBLE,
+}
+
+enum STATUS_PRODUCT {
+  NEW,
+  USED,
+  ALMOST_NEW,
+}
---
+++ structure/team.ts
@@ -0,0 +1,5 @@
+export type Team = {
+  id: string
+  // members posee 2 string, (2 usuarios)
+  members: string[]
+}
