5852dc0932a1a4942f11aea3bf9ed5ffc3f71406
Fernando Villalba <21fervillalbag@gmail.com>
2023-02-26 15:07:02 -0300

    delete: styles css on next.js

--- apps/breaded/src/user/entities/user.entity.ts
+++ apps/breaded/src/user/entities/user.entity.ts
@@ -23,16 +23,16 @@ export class User extends Document {
   password: string
 
   @Prop({ type: AVATAR_SCHEMA, _id: false })
-  avatar: { url_image: string is_approved: boolean updated_at: string }
+  avatar: { url_image: string; is_approved: boolean; updated_at: string }
 
   @Prop({ type: BANNER_SCHEMA, _id: false })
-  banner: { url_image: string updated_at: string }
+  banner: { url_image: string; updated_at: string }
 
   @Prop({ type: IS_VERIFIED_SCHEMA, _id: false })
-  is_verified: { is_valid: string updated_at: string }
+  is_verified: { is_valid: string; updated_at: string }
 
   @Prop({ type: IS_PREMIUM_SCHEMA, _id: false })
-  is_premium: { is_valid: boolean updated_at: string }
+  is_premium: { is_valid: boolean; updated_at: string }
 
   @Prop({ type: String })
   created_at: string
