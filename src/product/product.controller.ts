18477fb982e2c131282090a6f7c58c9b19e8a82b
Fernando Villalba <fernando.villalba@clt.com.py>
2023-03-14 09:01:39 -0300

    feat: new filter in product schema

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,37 +1,52 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+
 import { ProductService } from './product.service';
-import { CreateProductDto } from './dto/create-product.dto';
-import { UpdateProductDto } from './dto/update-product.dto';
+import { CreateProductDto, UpdateProductDto } from './dto';
+import { ProductDocument } from './entities/product.entity';
 
 @Controller('product')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
   @Post('create')
-  create(@Body() createProductDto: CreateProductDto) {
+  create(@Body() createProductDto: CreateProductDto): Promise<ProductDocument> {
     return this.productService.create(createProductDto);
   }
 
   @Get()
-  findAll() {
+  findAll(): Promise<ProductDocument[]> {
     return this.productService.findAll();
   }
 
-  @Get(':param/:value')
+  @Get('param/:param/:value')
   findOne(
     @Param('param') param: string,
     @Param('value') value: string
-  ) {
+  ): Promise<ProductDocument> {
     return this.productService.findOne(param, value);
   }
 
+  @Get('status-product/:status')
+  findManyByStatusProduct(
+    @Param('status') status: string,
+  ): Promise<ProductDocument[]> {
+    return this.productService.findManyByStatusProduct(status);
+  }
+
+  @Get('status-sale/:status')
+  findManyByStatusSale(
+    @Param('status') status: string,
+  ): Promise<ProductDocument[]> {
+    return this.productService.findManyByStatusSale(status);
+  }
+
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
+  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto): Promise<ProductDocument> {
     return this.productService.update(id, updateProductDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  remove(@Param('id') id: string): Promise<ProductDocument> {
     return this.productService.remove(id);
   }
 }
