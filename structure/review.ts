d6680d441a059f816687878097fc42873aa55b40
Fernando Villalba <21fervillalbag@gmail.com>
2023-03-20 22:03:46 -0300

    feat: ; added in structure objects

--- structure/review.ts
+++ structure/review.ts
@@ -1,10 +1,10 @@
 export type Review = {
-  id: string
-  id_vendor: string
-  id_buyer: string
-  stars_count: number
-  description: string
-  id_product: string
-  created_at: string
-  updated_at: string
-}
+  id: string;
+  id_vendor: string;
+  id_buyer: string;
+  stars_count: number;
+  description: string;
+  id_product: string;
+  created_at: string;
+  updated_at: string;
+};
--- structure/service.ts
+++ structure/service.ts
@@ -1,19 +1,19 @@
 export type Service = {
-  id: string
-  title: string
-  description: string
-  images: Image[]
-  price_start: number
+  id: string;
+  title: string;
+  description: string;
+  images: Image[];
+  price_start: number;
   location: {
-    city: string
-    department: string
-  }
-  created_at: string
-  updated_at: string
-}
+    city: string;
+    department: string;
+  };
+  created_at: string;
+  updated_at: string;
+};
 
 type Image = {
-  id: string
-  url_image: string
-  created_at: string
-}
+  id: string;
+  url_image: string;
+  created_at: string;
+};
