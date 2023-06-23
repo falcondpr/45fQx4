2fd670b174619c0c65654339d5886067e4bda5ea
Fernando Villalba <21fervillalbag@gmail.com>
2023-06-23 13:49:43 -0400

    feat: register/email screen ui

---
+++ apps/app/app/register/email/page.tsx
@@ -0,0 +1,119 @@
+'use client';
+
+import React, { useEffect, useState } from 'react';
+import NextLink from 'next/link';
+import * as yup from 'yup';
+import { Box, Button, Grid, Heading, Text, Link, Flex } from '@chakra-ui/react';
+import { Formik } from 'formik';
+import { useRouter } from 'next/navigation';
+
+import { Input } from '@sura/ui';
+import DotActive from '../../components/auth/DotActive';
+
+const registerValidationSchema = yup.object().shape({
+  email: yup
+    .string()
+    .email('Debe ser un correo válido')
+    .required('El correo es obligatorio'),
+});
+
+export default function Register() {
+  const router = useRouter();
+
+  // eslint-disable-next-line
+  const handleRegisterUser = (values: any): void => {
+    console.log({ values });
+  };
+
+  const [height, setHeight] = useState<number | string>(0);
+
+  useEffect(() => {
+    const innerHeight = window.innerHeight;
+    setHeight(innerHeight);
+  }, []);
+
+  return (
+    <Flex flexDir="column" height={height} justifyContent="space-between">
+      <Box>
+        <Grid
+          height="200px"
+          bgSize="cover"
+          bgPos="center"
+          width="100%"
+          bgImage="url('/images/bg-register.png')"
+          p="20px"
+          alignContent="flex-end"
+          gridTemplateRows="repeat(2, min-content)"
+        >
+          <Heading variant="base">Bienvenido!</Heading>
+          <Text>
+            Si aun no tienes
+            <br /> una cuenta debes registrarte
+          </Text>
+        </Grid>
+
+        <Box p="20px">
+          <Formik
+            validationSchema={registerValidationSchema}
+            initialValues={{
+              email: '',
+            }}
+            // eslint-disable-next-line
+            onSubmit={(values: any) => handleRegisterUser(values)}
+          >
+            {({ handleBlur, handleChange, values, handleSubmit, errors }) => (
+              <Box>
+                <Box>
+                  <Input
+                    label="email"
+                    value={values.email}
+                    onChange={handleChange('email')}
+                    onBlur={handleBlur('email')}
+                  />
+                  {errors.email && (
+                    <Text fontSize="14px" mt="-12px" mb="12px" color="red.400">
+                      {errors.email as string}
+                    </Text>
+                  )}
+                </Box>
+
+                <DotActive value={1} />
+
+                <Flex gap="16px" mt="20px">
+                  <Button
+                    flex="1"
+                    variant="base"
+                    color="brand.900"
+                    fontSize="18px"
+                    bgColor="white"
+                    shadow="none"
+                    border="1px solid"
+                    borderColor="brand.700"
+                    onClick={() => router.push('/register/principal')}
+                  >
+                    Volver
+                  </Button>
+                  <Button
+                    flex="1"
+                    variant="base"
+                    fontSize="18px"
+                    onClick={() => handleSubmit()}
+                  >
+                    Siguiente
+                  </Button>
+                </Flex>
+              </Box>
+            )}
+          </Formik>
+        </Box>
+      </Box>
+
+      <Text pb="32px" color="brand.700" textAlign="center">
+        Ya tienes una cuenta?{' '}
+        <Link href="/login" as={NextLink} textDecor="underline">
+          Inicia sesion
+        </Link>
+      </Text>
+    </Flex>
+  );
+}
