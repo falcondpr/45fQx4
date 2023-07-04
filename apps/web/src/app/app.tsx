fd17535445c2aa275996bafc8933c5d95685b6af
Fernando Villalba <21fervillalbag@gmail.com>
2023-07-04 19:55:53 -0400

    feat: create web app with vite and tailwindcss

---
+++ apps/web/src/app/app.tsx
@@ -0,0 +1,9 @@
+export function App() {
+  return (
+    <div className="grid place-items-center h-screen bg-black">
+      <p className="text-white text-2xl">Hello world from tailwind</p>
+    </div>
+  );
+}
+
+export default App;
